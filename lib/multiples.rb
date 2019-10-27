c16a8a8b0fe2b64c9933e79f5ec25ff816ceab48
jastaffen <jack.staffen@gmail.com>
2019-10-26 22:48:28 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    factor_arr = (1...limit).select { |factor| factor % 3 == 0 || factor % 5 == 0 } 
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).reduce(0) { |sum, num| sum + num }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        factored_arr = (1...self.limit).select { |factor| factor % 3 == 0 || factor % 5 == 0 }
+    end
+
+    def sum_multiples
+        self.collect_multiples.reduce(0) { |sum, num| sum + num }
+    end
+end
